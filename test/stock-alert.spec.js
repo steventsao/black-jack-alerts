8365ed44f373967a3c6897b5071eefddeac9126d
steventsao <steventsao713@gmail.com>
2016-04-14 17:20:44 -0700

    Setup db and assertions

---
+++ test/stock-alert.spec.js
@@ -0,0 +1,35 @@
+import expect from 'expect';
+import { getFriendsListForUser, getTradeTransactionsForUser } from '../stock-alert.js';
+import { uniq } from 'lodash';
+
+describe('Stocks Your Friends Are Trading', () => {
+  describe('getFriendsListForUser()', () => {
+    it('should return an array', () => {
+      expect(Array.isArray(getFriendsListForUser('A1234'))).toEqual(true);
+    });
+
+    it('should throw with invalid input', () => {
+      expect(getFriendsListForUser).withArgs().toThrow(/Invalid input/);
+    });
+
+    xit('should not have duplicate user IDs', () => {
+      let userIds = getFriendsListForUser('A1234');
+      let uniqUserIds = uniq(userIds);
+      expect(userIds.length).toEqual(uniqUserIds.length);
+    });
+  });
+
+  describe('getTradeTransactionsForUser()', () => {
+    it('should return an array', () => {
+      expect(Array.isArray(getTradeTransactionsForUser('A1234'))).toEqual(true);
+    });
+
+    it('should throw with invalid input', () => {
+      expect(getTradeTransactionsForUser).withArgs().toThrow(/Invalid input/);
+    });
+
+    it('item should contain three elements', () => {
+      expect(getTradeTransactionsForUser('A1234').split(',').length).toEqual(3);
+    });
+  });
+});
